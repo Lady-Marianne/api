15b933060199f98d40ecc8ac0615abb67f1b5bad
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-11-25 14:23:00 -0300

    Implementar reglas de negocio.

--- src/main/java/med/voll/api/domain/consulta/ReservaDeConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsulta.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.consulta;
 
-import jakarta.validation.ValidationException;
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,17 +19,36 @@ public class ReservaDeConsulta {
 
     public void reservar(DatosReservaConsulta datos){
 
-        var medico = medicoRepository.findById(datos.idMedico()).get();
+        if(!pacienteRepository.existsById(datos.idPaciente())) {
+            throw new ValidacionException("No existe un paciente con la Id informada.");
+        }
+
+        if(datos.idMedico() != null && !medicoRepository.existsById(datos.idMedico())) {
+            throw new ValidacionException("No existe un médico con la Id informada.");
+        }
+
+        var medico = elegirMedico(datos);
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, datos.fecha(), null);
         consultaRepository.save(consulta);
 
     }
 
+    private Medico elegirMedico(DatosReservaConsulta datos) {
+        if(datos.idMedico() != null) {
+            return medicoRepository.getReferenceById(datos.idMedico());
+        }
+        if(datos.especialidad() == null)   {
+            throw new ValidacionException("Es necesario elegir una especialidad cuando no se elige el médico.");
+        }
+        return medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(datos.especialidad(), datos.fecha());
+    }
+
     public void cancelar(DatosCancelamientoConsulta datos) {
         if (!consultaRepository.existsById(datos.idConsulta())) {
-            throw new ValidationException("Id de la consulta informado no existe!");
+            throw new ValidacionException("No existe la consulta con la Id informada.");
         }
+
         var consulta = consultaRepository.getReferenceById(datos.idConsulta());
         consulta.cancelar(datos.motivo());
     }
